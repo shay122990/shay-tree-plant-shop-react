02ded69d0b078f30bccdcd642623863163e2f4c4
Shay <moeshakasanova@gmail.com>
2024-06-13 15:42:53 +0400

    added bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,8 @@
-import { plants } from "./plants-data";
-
+import PlantCard from "./components/plant-card/plant-cad.components";
 function App() {
   return (
     <>
-      <div>
-        <h1>Plant Store</h1>
-        <div>
-          {plants.map((plant) => (
-            <div key={plant.id} className="plant-item">
-              <img src={plant.image} alt={plant.name} />
-              <h3>{plant.name}</h3>
-              <p>{plant.description}</p>
-              <p>Price: ${plant.price.toFixed(2)}</p>
-            </div>
-          ))}
-        </div>
-      </div>
+      <PlantCard />
     </>
   );
 }
---
+++ src/components/plant-card/plant-cad.components.jsx
@@ -0,0 +1,28 @@
+import { plants } from "../../plants-data";
+const PlantCard = () => {
+  return (
+    <div className="container">
+      <h1 className="text-center my-4">Plant Store</h1>
+      <div className="row row-cols-1 row-cols-md-3 g-4">
+        {plants.map((plant) => (
+          <div key={plant.id} className="col">
+            <div className="card h-100">
+              <img
+                src={plant.image}
+                className="card-img-top"
+                alt={plant.name}
+              />
+              <div className="card-body">
+                <h5 className="card-title">{plant.name}</h5>
+                <p className="card-text">{plant.description}</p>
+                <p className="card-text">Price: ${plant.price.toFixed(2)}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default PlantCard;
