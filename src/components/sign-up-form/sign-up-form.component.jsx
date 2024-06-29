7e2a2b0e937e83f4e8ca9aa39145c3fdc58c0eb9
Shay <moeshakasanova@gmail.com>
2024-06-29 20:53:27 +0400

    sign-up component

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,5 +1,41 @@
+import "./sign-up-form.styles.css";
 const SignUpForm = () => {
-  return <div></div>;
+  return (
+    <div className="sign-up-container">
+      <h1>Register</h1>
+      <form>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+          />
+        </div>
+        <div className="sign-up-button-container">
+          <button type="submit" className="btn btn-light">
+            Sign Up
+          </button>
+          <button type="button" className="btn btn-light">
+            Register With Google
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default SignUpForm;
