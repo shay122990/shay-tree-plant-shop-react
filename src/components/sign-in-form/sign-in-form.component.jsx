c95233872ce0c8c94538546d92d0023b1f74bc07
Shay <moeshakasanova@gmail.com>
2024-07-18 21:56:38 +0400

    signInWithEmailAndPassword and clean up of signIn form component

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,22 +1,64 @@
 import "./sign-in-form.styles.css";
+import { useState } from "react";
 import {
   signInWithGooglePopup,
   createUserDocumentFromAuth,
+  signInAuthUserWithEmailAndPassword,
 } from "../../utils/firebase.utils";
 import FormInput from "../form-input/form-input.component";
 import Button from "../button/button.component";
 
+const defaultFormFields = {
+  email: "",
+  password: "",
+};
 const SignInForm = () => {
-  const logGoogleUser = async () => {
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { email, password } = formFields;
+
+  const resetFormFields = () => {
+    setFormFields(defaultFormFields);
+  };
+  const signInWithGoogle = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRefrence = await createUserDocumentFromAuth(user);
     console.log(userDocRefrence);
   };
 
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await signInAuthUserWithEmailAndPassword(
+        email,
+        password
+      );
+      console.log(response);
+      resetFormFields();
+    } catch (error) {
+      switch (error.code) {
+        case "auth/wrong-password":
+          alert("incorrect password for email");
+          break;
+        case "auth/user-not-found":
+          alert("no user associated with this email");
+          break;
+        default:
+          console.log(error);
+      }
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormFields({ ...formFields, [name]: value });
+  };
+
   return (
     <div className="sign-in-container">
       <h1>Sign In</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <FormInput
           label="Email address"
           type="email"
@@ -25,6 +67,7 @@ const SignInForm = () => {
           aria-describedby="emailHelp"
           autoComplete="email"
           required
+          onChange={handleChange}
         />
         <FormInput
           label="Password"
@@ -33,12 +76,13 @@ const SignInForm = () => {
           name="password"
           autoComplete="current-password"
           required
+          onChange={handleChange}
         />
         <div className="sign-in-button-container">
           <Button type="submit" buttonType="generic">
             Sign In
           </Button>
-          <Button type="submit" buttonType="google" onClick={logGoogleUser}>
+          <Button type="button" buttonType="google" onClick={signInWithGoogle}>
             Sign In With Google
           </Button>
         </div>
