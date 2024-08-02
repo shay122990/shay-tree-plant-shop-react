5f4947f356f668f2e7dd4776e5c035befba78dba
Shay <moeshakasanova@gmail.com>
2024-08-02 19:42:53 +0400

    added cartCount to cartIcon

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,12 +1,14 @@
 import "./cart-dropdown.styles.css";
-import Button from "../button/button.component";
+
 import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import { CartContext } from "../../contexts/cart.context";
 import CartItem from "../cart-item/cart-item.component";
+import Button from "../button/button.component";
 
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
-
+  const navigate = useNavigate();
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items">
@@ -16,7 +18,9 @@ const CartDropdown = () => {
           cartItems.map((item) => <CartItem key={item.id} cartItem={item} />)
         )}
       </div>
-      <Button buttonType="cart">Go To Checkout</Button>
+      <Button buttonType="cart" onClick={navigate("/checkout")}>
+        Go To Checkout
+      </Button>
     </div>
   );
 };
--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -21,17 +21,25 @@ export const CartContext = createContext({
   setIsCartOpen: () => {},
   cartItems: [],
   addItemToCart: () => {},
+  cartCount: 0,
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
   const [cartItems, setCartItems] = useState([]);
+  const cartCount = cartItems.reduce((total, item) => total + item.quantity, 0);
 
   const addItemToCart = (productToAdd) => {
     setCartItems(addCartItem(cartItems, productToAdd));
   };
 
-  const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems };
+  const value = {
+    isCartOpen,
+    setIsCartOpen,
+    addItemToCart,
+    cartItems,
+    cartCount,
+  };
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
 
