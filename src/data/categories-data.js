6fa2d6c3a2c27c93dda541461edf30d13f0f8cc8
Shay <moeshakasanova@gmail.com>
2024-12-09 18:02:23 +0400

    created best seller category

--- src/data/categories-data.js
+++ src/data/categories-data.js
@@ -37,4 +37,10 @@ export const categories = [
     image:
       "https://firebasestorage.googleapis.com/v0/b/shay-tree-db-bb835.appspot.com/o/plants%2Fimg7.jpg?alt=media&token=3a32f8c6-c1ae-4338-94c5-b1ce04140d85",
   },
+  {
+    name: "Best Sellers",
+    plantIds: [1, 3, 7, 10, 19, 30],
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/shay-tree-db-bb835.appspot.com/o/plants%2Fimg4.jpg?alt=media&token=0a7a5ff0-d5bf-4f34-9b3b-f26eeee81d11",
+  },
 ];
