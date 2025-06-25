62452c4c63eddce208edb60e31f629325b781adc
shay asanova <moeshakasanova@gmail.com>
2025-06-25 11:49:10 +0400

    refactored navbar on mobile and used react-icons

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -1,5 +1,5 @@
 import "./navbar.styles.css";
-import { useContext } from "react";
+import { useState, useContext } from "react";
 import { UserContext } from "../../contexts/user.context";
 import { CartContext } from "../../contexts/cart.context";
 import { signOutUser } from "../../utils/firebase.utils";
@@ -7,60 +7,86 @@ import { Link, Outlet } from "react-router-dom";
 import logo from "../../assets/shays-tree.jpg";
 import CartIcon from "../cart-icon/cart-icon.component";
 import CartDropdown from "../cart-dropdown/cart-dropdown.component";
+import { FiMenu } from "react-icons/fi";
+
 const Navbar = () => {
   const { currentUser } = useContext(UserContext);
   const { isCartOpen } = useContext(CartContext);
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => setMenuOpen((prev) => !prev);
+  const closeMenu = () => setMenuOpen(false);
+
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-light">
-        <div className="container-fluid d-flex justify-content-between mx-3">
+      <nav className="navbar navbar-light px-3 py-2">
+        <div className="container-fluid d-flex justify-content-between align-items-center">
           <Link className="navbar-brand" to="/">
             <img src={logo} alt="Shay's Tree Logo" />
           </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <Link className="nav-link" to="/">
-                  Home
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/plants">
-                  Plants
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/about">
-                  About
-                </Link>
-              </li>
-              {currentUser ? (
-                <span className="nav-link" onClick={signOutUser}>
-                  Sign Out
-                </span>
-              ) : (
-                <li className="nav-item">
-                  <Link className="nav-link" to="/auth">
-                    Sign In
-                  </Link>
-                </li>
-              )}
-              <CartIcon />
-            </ul>
+
+          <div className="d-lg-none">
+            <button className="menu-toggle" onClick={toggleMenu}>
+              <FiMenu size={32} />
+            </button>
           </div>
-          {isCartOpen && <CartDropdown />}
+
+          <div className="d-none d-lg-flex gap-4 align-items-center">
+            <Link className="nav-link" to="/">
+              Home
+            </Link>
+            <Link className="nav-link" to="/plants">
+              Plants
+            </Link>
+            <Link className="nav-link" to="/about">
+              About
+            </Link>
+            {currentUser ? (
+              <span className="nav-link" onClick={signOutUser}>
+                Sign Out
+              </span>
+            ) : (
+              <Link className="nav-link" to="/auth">
+                Sign In
+              </Link>
+            )}
+            <CartIcon />
+          </div>
         </div>
       </nav>
+
+      <div className={`mobile-menu ${menuOpen ? "open" : ""}`}>
+        <button className="close-btn" onClick={closeMenu}>
+          ×
+        </button>
+        <Link to="/" className="nav-link" onClick={closeMenu}>
+          Home
+        </Link>
+        <Link to="/plants" className="nav-link" onClick={closeMenu}>
+          Plants
+        </Link>
+        <Link to="/about" className="nav-link" onClick={closeMenu}>
+          About
+        </Link>
+        {currentUser ? (
+          <span
+            className="nav-link"
+            onClick={() => {
+              signOutUser();
+              closeMenu();
+            }}
+          >
+            Sign Out
+          </span>
+        ) : (
+          <Link to="/auth" className="nav-link" onClick={closeMenu}>
+            Sign In
+          </Link>
+        )}
+        <CartIcon />
+      </div>
+
+      {isCartOpen && <CartDropdown />}
       <Outlet />
     </>
   );
